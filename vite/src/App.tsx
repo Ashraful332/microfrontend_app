34736c64484fd758c832b5e1ac126156e4b0f061
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-08-15 10:31:01 +0600

    update the page

--- vite/src/App.tsx
+++ vite/src/App.tsx
@@ -1,11 +1,13 @@
+import { Link } from 'react-router'
 import './App.css'
 
 function App() {
 
 
   return (
-    <div>
-      Hello world
+    <div className='app-main'>
+      <h2>Pages</h2>
+      <Link to='/' className='link-page'>glass page</Link>
     </div>
   )
 }
