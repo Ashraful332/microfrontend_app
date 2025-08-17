b56010ee30916241c10e2a7abcf443e7ab1b9281
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-08-17 13:48:44 +0600

    uplode the vm

---
+++ src/app/vm/page.tsx
@@ -0,0 +1,9 @@
+export default function Vm() {
+  return (
+    <div>
+      <h1>Micro Frontend Container</h1>
+      <iframe src="https://pronoy.vercel.app/" width="600" height="400" />
+      <iframe src="http://localhost:5174" width="600" height="400" />
+    </div>
+  )
+}
