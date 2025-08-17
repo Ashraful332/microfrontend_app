8820ccbdad52dcfae91c16b8ae0b271a00749e4e
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-08-17 13:50:39 +0600

    add the server

--- src/app/vm/page.tsx
+++ src/app/vm/page.tsx
@@ -1,9 +1,7 @@
 export default function Vm() {
   return (
     <div>
-      <h1>Micro Frontend Container</h1>
-      <iframe src="https://pronoy.vercel.app/" width="600" height="400" />
-      <iframe src="http://localhost:5174" width="600" height="400" />
+      <iframe src="https://pronoy.vercel.app/" className="w-[100vw] h-[100vh]  " />
     </div>
   )
 }
